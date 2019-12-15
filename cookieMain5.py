1fb7cc2d8810b3e2d8cbcad99fd0f5a043fe3fb1
Manish Sharma <37637814+manish-sharma7@users.noreply.github.com>
2019-12-16 02:09:15 +0530

    Update cookieMain5.py

--- cookieMain5.py
+++ cookieMain5.py
@@ -1,1 +1,29 @@
-#Created By Manish Sharma
+import os
+import picamera
+from datetime import datetime
+from subprocess import call
+from time import sleep
+from cookieClient4 import send
+from time import time
+
+picPath = "/home/pi/Desktop/"
+
+def captureImage(picPath):
+    # Generate the picture's name
+    picName = 'image.jpg'
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.capture(picPath + picName)
+    print("We have taken a picture.")
+    return picName
+
+def main():
+    while True:
+        picName = captureImage(picPath)
+        print("Took a picture")
+        completePath = picPath + picName
+        print(completePath)
+        send(completePath)
+        print("Image has been sent to PC :)")
+        os.remove(completePath)
+        break
