217b114e299072084b8f9ec1dde2a8c1449028f4
Manish Sharma <37637814+manish-sharma7@users.noreply.github.com>
2019-12-15 17:28:02 +0530

    Update cookieMain3.py

--- cookieMain3.py
+++ cookieMain3.py
@@ -1,1 +1,32 @@
-#Created By Manish Sharma
+import picamera
+from datetime import datetime
+from subprocess import call
+from time import sleep
+from cookieClient3 import backup
+
+picPath = "/home/pi/Desktop"
+sleepTime = 3
+triggerTemp = 20
+
+def captureImage(currentTime, picPath):
+    # Generate the picture's name
+    picName = currentTime.strftime("%Y.%m.%d-%H%M%S") + '.jpg'
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.capture(picPath + picName)
+    print("We have taken a picture.")
+    return picName
+
+def getTime():
+    # Fetch the current time
+    currentTime = datetime.now()
+    return currentTime
+while True:
+    currentTime = getTime()
+    picName = captureImage(currentTime, picPath)
+    print("Took a picture")
+    completePath = picPath + picName
+    print(completePath)
+    backup(completePath)
+    print("Everything should be backed up now.")
+    break
